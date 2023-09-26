5c69b0703e37361428e9fd8a4e75b1d9f93749c5
Htain Lin Shwe <saturngod@gmail.com>
2023-09-26 14:06:08 -0500

    button style done. add animation for change the picture

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while (randomNumber == imageNumber)
+        imageNumber = randomNumber
+    }
     var body: some View {
         ZStack {
             CustomBackgroundView()
@@ -43,11 +56,31 @@ struct CardView: View {
                             LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
                         )
                         .frame(width: 256,height: 256)
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                     .scaledToFit()
+                    .animation(.default, value: imageNumber)
                 }
                 //MARK: - FOOTER
+                
+                Button {
+                    //Action:: Generate a random number
+                    print("The button was pressed")
+                    randomImage()
+                } label: {
+                    Text("Explorer More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.customGreenLight,.customGreenMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25),
+                                radius: 0.25,x:1,y:2
+                        )
+                }
+                .buttonStyle(GradientButton())
+                
+                
             }
         } //: CARD
         .frame(width: 320,height: 570)
